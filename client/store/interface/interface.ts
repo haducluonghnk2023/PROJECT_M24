26ef576bbf0b364703c566b90db825dcf19f7200
haducluonghnk2023 <haducluongdz@gmail.com>
2024-07-05 10:22:19 +0700

    ad

---
+++ client/store/interface/interface.ts
@@ -0,0 +1,17 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string; 
+}
+export interface Course {
+    courseName: string;
+    examDate: string;
+  }
+  
+  export interface CoursesState {
+    courses: Course[];
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null;
+  }
+  
+
